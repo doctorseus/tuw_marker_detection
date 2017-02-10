bc54630c40cb1cfadc4ad6b53f64eae928b24865
doctorseus <doctorseus@devlabs.pro>
2017-02-10 22:23:01 +0100

    Cleaned up python listener and added visual helper for marker messages.

--- tuw_marker_benchmark/src/listener.py
+++ tuw_marker_benchmark/src/listener.py
@@ -1,11 +1,39 @@
 #!/usr/bin/env python
 import rospy
 import math
-from std_msgs.msg import String
 from tf2_ros import TransformListener, Buffer
 from tf2_msgs.msg import TFMessage
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
+
+class VisualHelper:
+
+    @staticmethod
+    def createMarker(frame, namespace, id, scale, (r, g, b, a)):
+        marker = Marker()
+        marker.header.frame_id = frame
+        marker.header.stamp = rospy.Time.now()
+        marker.ns = namespace
+        marker.action = Marker.ADD
+
+        marker.pose.orientation.w = 1.0
+        marker.id = id
+
+        marker.scale.x = scale
+        marker.scale.y = scale
+
+        marker.color.r = r
+        marker.color.g = g
+        marker.color.b = b
+        marker.color.a = a
+        return marker
+
+    @staticmethod
+    def createPoints(frame, namespace, id, scale, color):
+        marker = VisualHelper.createMarker(frame, namespace, id, scale, color)
+        marker.type = Marker.POINTS
+        return marker
+
 
 class BenchmarkNode:
 
@@ -23,32 +51,12 @@ class BenchmarkNode:
     def tf_callback(self, data):
         #rospy.loginfo('%s', self.tfBuf.all_frames_as_string())
 
-        #t = self.tfb.getLatestCommonTime("/p3dx/camera", "/p3dx/odom")
         try:
             tf = self.tfb.lookup_transform("p3dx/odom", "p3dx/camera", rospy.Time(0))
             rospy.loginfo('%s', tf)
 
             # http://wiki.ros.org/rviz/Tutorials/Markers%3A%20Points%20and%20Lines
-            points = Marker()
-            points.header.frame_id = "p3dx/odom"
-            points.header.stamp = rospy.Time.now()
-
-            points.ns = "points_and_lines"
-            points.action = Marker.ADD
-
-            points.pose.orientation.w = 1.0
-
-            points.id = 0
-
-            points.type = Marker.POINTS
-
-            # POINTS markers use x and y scale for width/height respectively
-            points.scale.x = 0.1
-            points.scale.y = 0.1
-
-            # Points are green
-            points.color.g = 1.0
-            points.color.a = 1.0
+            marker = VisualHelper.createPoints("p3dx/odom", "camera", 1, 0.2, (1, 0, 0, 1))
 
             '''
             f = 0.0
@@ -61,46 +69,21 @@ class BenchmarkNode:
                 p.y = y
                 p.z = 0
 
-                points.points.append(p)
+                marker.points.append(p)
             '''
             p = Point()
             p.x = tf.transform.translation.x
             p.y = tf.transform.translation.y
             p.z = 0
-            points.points.append(p)
+            marker.points.append(p)
 
-            self.vizPub.publish(points)
+            self.vizPub.publish(marker)
         except Exception as e:
             rospy.logerr(e.message)
 
-
-
-        #rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
 
 if __name__ == '__main__':
     rospy.init_node('markerBenchmarkNode', anonymous=True)
     node = BenchmarkNode()
     node.run()
     rospy.spin()
-
-'''
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
-
-def listener():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
-
-    rospy.Subscriber('chatter', String, callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
-
-if __name__ == '__main__':
-    listener()
-'''
