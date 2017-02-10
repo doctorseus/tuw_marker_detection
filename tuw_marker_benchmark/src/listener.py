c27264d5e58295717d56bb79193fe011525e0a66
doctorseus <doctorseus@devlabs.pro>
2017-02-10 17:14:11 +0100

    Added simple point vizualization for testing.

--- tuw_marker_benchmark/src/listener.py
+++ tuw_marker_benchmark/src/listener.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import rospy
+import math
 from std_msgs.msg import String
 from tf2_ros import TransformListener, Buffer
 from tf2_msgs.msg import TFMessage
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
 
 class BenchmarkNode:
 
@@ -10,13 +13,60 @@ class BenchmarkNode:
         pass
 
     def run(self):
-        self.tfBuf = Buffer()
-        self.tfSub = TransformListener(self.tfBuf)
+        self.tfb = Buffer()
+        self.tfSub = TransformListener(self.tfb)
         rospy.Subscriber("/tf", TFMessage, self.tf_callback)
+
+        self.vizPub = rospy.Publisher("benchmark_visualization", Marker, queue_size=10)
 
 
     def tf_callback(self, data):
-        rospy.loginfo('%s', self.tfBuf.all_frames_as_string())
+        #rospy.loginfo('%s', self.tfBuf.all_frames_as_string())
+
+        #t = self.tfb.getLatestCommonTime("/p3dx/camera", "/p3dx/odom")
+        try:
+            tf = self.tfb.lookup_transform("p3dx/camera", "p3dx/odom", rospy.Time(0))
+            rospy.loginfo('%s', tf)
+
+            # http://wiki.ros.org/rviz/Tutorials/Markers%3A%20Points%20and%20Lines
+            points = Marker()
+            points.header.frame_id = "p3dx/odom"
+            points.header.stamp = rospy.Time.now()
+
+            points.ns = "points_and_lines"
+            points.action = Marker.ADD
+
+            points.pose.orientation.w = 1.0
+
+            points.id = 0
+
+            points.type = Marker.POINTS
+
+            # POINTS markers use x and y scale for width/height respectively
+            points.scale.x = 0.2
+            points.scale.y = 0.2
+
+            # Points are green
+            points.color.g = 1.0
+            points.color.a = 1.0
+
+            f = 0.0
+            for i in range(0, 100):
+                y = 5 * math.sin(f + i / 100.0 * 2 * math.pi)
+                z = 5 * math.cos(f + i / 100.0 * 2 * math.pi)
+
+                p = Point()
+                p.x = i - 50
+                p.y = y
+                p.z = z
+
+                points.points.append(p)
+
+            self.vizPub.publish(points)
+        except Exception as e:
+            rospy.logerr(e.message)
+
+
 
         #rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
 
