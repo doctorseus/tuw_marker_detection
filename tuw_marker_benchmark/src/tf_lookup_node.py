0b0cde664735c58f4c238feb5b64c65243499937
doctorseus <doctorseus@devlabs.pro>
2017-03-09 11:53:50 +0100

    Launch file update.

--- tuw_marker_benchmark/src/tf_lookup_node.py
+++ tuw_marker_benchmark/src/tf_lookup_node.py
@@ -12,6 +12,12 @@ class TfLookupNode:
         self.tfSub = None
         self.tf_records = []
 
+        # Read parameters
+        self.tf_target_frame = rospy.get_param("~tf_target_frame", None)
+        self.tf_source_frame = rospy.get_param("~tf_source_frame", None)
+        if self.tf_target_frame is None or self.tf_source_frame is None:
+            raise ValueError("Parameter tf_target_frame or/and tf_source_frame not set")
+
     def run(self):
         self.tfSub = TransformListener(self.tfb)
         rospy.Subscriber("/tf", TFMessage, self.tf_callback)
@@ -19,8 +25,8 @@ class TfLookupNode:
     def tf_callback(self, msg):
         try:
             timeout = 1.0
-            tfs = self.tfb.lookup_transform("base_link", "camera_inv", rospy.Time(0), rospy.Duration(timeout))
-
+            tfs = self.tfb.lookup_transform(self.tf_target_frame, self.tf_source_frame, rospy.Time(0), rospy.Duration(timeout))
+            print(tfs)
             self.tf_records.append(tfs.transform)
 
             if(len(self.tf_records) > 50):
@@ -38,6 +44,13 @@ class TfLookupNode:
 
 if __name__ == '__main__':
     rospy.init_node('tfLookupNode', anonymous=True)
-    node = TfLookupNode()
-    node.run()
-    rospy.spin()
+    try:
+        node = TfLookupNode()
+        node.run()
+        rospy.spin()
+    except StandardError as e:
+        rospy.logerr("Failed to start %s: %s" % (rospy.get_name(), e.message))
+        rospy.signal_shutdown("Failed to start %s: %s" % (rospy.get_name(), e.message))
+        exit(1)
+
+
